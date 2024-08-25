e898f2342b06bca1db589ffe6502835376f20081
radikon <radikon@gmail.com>
2024-08-25 16:17:19 -0500

    new github action for bug comments

--- ci.py
+++ ci.py
@@ -2,6 +2,3 @@ import wandb
 import ghapi
 
 print(f'The version of WandB is: {wandb.__version__}')
-
-# we're going to do the first assignment in this same file. The first assignment is to
-# make a GitHub Action workflow that labels a PR with "bug" after you make the comment "/bug"
